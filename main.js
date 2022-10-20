2594298f3c0245c0880a13b6a1a29b5f152098a8
Will Spencer <willspencer@me.com>
2022-10-19 23:51:50 -0600

    Residents populating

--- main.js
+++ main.js
@@ -6,14 +6,16 @@
  residentsBtn.addEventListener('click', () => {
     axios.get(baseURL + "/api/planets/?search=alderaan")
     .then((res) => {
-        console.log(res.data.results[0].residents)
+        // console.log(res.data.results[0].residents)
         let residents = res.data.results[0].residents
         for (let i = 0; i < residents.length; i++){
            axios.get(residents[i])
            .then((res) => {
-            // const name = document.createElement("h2");
-            // name.innerText = res.data.name
-            // res.data.name.appendChild(name)
+            let residents = res.data.name
+            const h2 = document.getElementById("residents-list")
+            let residentsList = document.createElement('h2')
+            residentsList.textContent = residents
+            h2.appendChild(residentsList)
             console.log(res.data.name)
            })
         }
